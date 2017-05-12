eb0ba031c4473342ada43c2a8ae3a5edf9c72755
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-11 18:45:50 -0700

    createToken method

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  DB_URL: process.env.DB_URL || 'mongodb://localhost/flofun',
+  JWT_SECRET: process.env.JWT_SECRET || 'dfghlkdfgh40586745608dkfjhdsfklgjhsdf456934587',
+}
+
+export default config
