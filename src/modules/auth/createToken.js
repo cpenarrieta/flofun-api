a4879f650970e1cb1e04084ed71697578a23517a
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-11 19:15:39 -0700

    fix config path

--- src/modules/auth/createToken.js
+++ src/modules/auth/createToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
-import config from '../../../config/config'
+import config from '../../config/config'
 
 export const createToken = args =>
   jwt.sign({ id: args._id }, config.JWT_SECRET)
