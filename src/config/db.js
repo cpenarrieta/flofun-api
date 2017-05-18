56b565e763f72b1ed7ff7df93f4f19484ae884ab
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-17 21:05:27 -0700

    add webpack2

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,13 @@ import config from './config'
 
 export default () => {
   mongoose.Promise = global.Promise
-  mongoose.connect(config.DB_URL)
+
+  try {
+    mongoose.connect(config.DB_URL);
+  } catch (err) {
+    mongoose.createConnection(config.DB_URL);
+  }
+
   mongoose.set('debug', true)
   mongoose.connection
     .once('open', () => console.log('mongoose running'))
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dbConfig from './config/db'
+import config from './config/config'
 import middlewareConfig from './config/middleware'
 import {
   FlowerRoutes,
@@ -21,13 +22,15 @@ app.use('/api', [
   StoreRoutes,
 ])
 
-const PORT = process.env.PORT || 8080
-
 /* eslint-disable no-console */
-app.listen(PORT, err => {
+app.listen(config.PORT, err => {
   if (err) {
     console.error(err)
   } else {
-    console.log(`API listen to port: ${PORT}`)
+    console.log(`
+      Server running on port: ${config.PORT}
+      Running on ${process.env.NODE_ENV}
+      ---
+    `)
   }
 })
